795c1a8e3ebf28d28d66aa98cd713ee4c2723de0
nicolassoam <nsm4456@gmail.com>
2023-05-07 13:40:27 -0300

    ta entrando no for

--- imprimeVetor.asm
+++ imprimeVetor.asm
@@ -1,9 +1,9 @@
 .data
-    vet: .word 0,0,0
+    vet: .word 0:3
 .text
-    li $s0, 5
-    li $s1, 6
-    li $s2, 3
+    li $s0, 4
+    li $s1, 8
+    li $s2, 12
 
 li $t0, 0
 
@@ -18,41 +18,26 @@ li $a1, 3
 jal imprimeVetor        
 
 
-printf:
-
-
 # receives a vector and it's size and prints it
 imprimeVetor:
-    addi $sp, $sp, -12
-    sw $ra, 0($sp)
-    sw $s0, 4($sp)
-    sw $s1, 8($sp)
-
+    # indice do for
     li $t0, 0
     
     add $t1, $zero,$a0
     add $s1, $zero,$a1
-
+	
     #vet
-    lbu $s0, 0($t1)
-
-
-    # copia vetor para $a1
-    move $a1, $s0
+    lw $s0, 0($a0)
 
     for:
         # recalcula posição do vetor
         add $t3, $t1, $t0
-        # Loading %d on $a0
-        # %
-        li $t3, 37
-        sb $a0, 0($t3)
-        # d
-        li $t3, 100
-        sb $a0, 1($t3)
-	
-        jal printf
-        addi $t0, $t0, 1
+  
+        sw $t3, 1($a0)
+        addi $v0, $zero, 1
+        addi $t0, $t0, 4
+        li $v0,1
+        syscall
         bgt $t0, $s1, fim_for
         j for
         
@@ -61,12 +46,8 @@ imprimeVetor:
         sw $a1, 0($t1)
         li $t1,110
         sw $a1, 1($t1)
-        jal printf
+
         
-    lw $ra, 0($sp)
-    lw $s0, 4($sp)
-    lw $s1, 8($sp)
-    addi $sp, $sp, 12
     jr $ra
         
 
