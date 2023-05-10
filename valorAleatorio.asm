e099d7b67ce6c6e65cff5b7d6c713f86b847f6ea
marialuisarg <marialuisa@ice.ufjf.br>
2023-05-10 15:33:58 -0300

    corrigindo comentarios e mudando t pra s

--- valorAleatorio.asm
+++ valorAleatorio.asm
@@ -3,33 +3,33 @@
 .text	
 	.main:
 		#inserindo valores para a,b,c,d,e
-		#assumindo a = $t0, b = $t1, c = $t2, d = $t3, e = $t4
-		li $t0, 10
-		li $t1, 3
-		li $t2, 2
-		li $t3, 3
-		li $t4, 1
+		#assumindo a = $t0, b = $t1, c = $t2, d = $t3, e = $t4 (valores aleatorios para teste da função)
+		li $s0, 10
+		li $s1, 3
+		li $s2, 2
+		li $s3, 3
+		li $s4, 1
 		
 		#passando os argumentos
-		move $a0, $t0       # 0($fp) reservado na pilha
-		move $a1, $t1       # 4($fp) reservado na pilha
-		move $a2, $t2       # 8($fp) reservado na pilha
-		move $a3, $t3       # 12($fp) reservado na pilha
+		move $a0, $s0       # 0($sp) reservado na pilha
+		move $a1, $s1       # 4($sp) reservado na pilha
+		move $a2, $s2       # 8($sp) reservado na pilha
+		move $a3, $s3       # 12($sp) reservado na pilha
 		add  $sp, $sp, -20
-		sw   $t4, 16($sp)   # armazena o 5o argumento na pilha
+		sw   $s4, 16($sp)   # armazena o 5o argumento na pilha
 		
 		jal  valorAleatorio
 		jal  FIM
 
 	valorAleatorio:
-		mult 	$a0, $a1   	    # a * b
+		mult 	$a0, $a1        # a * b
 		mflo 	$t0
-		add 	$t1, $t0, $a2	# a * b + c
-		div 	$t1, $a3	    # (a * b + c) % d
+		add 	$t1, $t0, $a2   # a * b + c
+		div 	$t1, $a3        # (a * b + c) % d
 		mfhi 	$t2
-		sub 	$v1, $t2, $t4	# (a * b + c) % d - e
+		sub 	$v1, $t2, $s4   # (a * b + c) % d - e
 	
-		jr	    $ra		        # retorna para main
+		jr      $ra             # retorna para main
 	
 	FIM: 
 		li $v0, 10
