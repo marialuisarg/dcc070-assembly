b384bcb89ef078c78f9f0ff2525829d8f4da0d92
nicolassoam <nsm4456@gmail.com>
2023-05-12 11:05:37 -0300

    agora funciona.

--- inicializaVetor.asm
+++ inicializaVetor.asm
@@ -23,6 +23,11 @@
     add $a0, $t3, $zero
     syscall
     
+    la $a0, vet
+    li $a1, 20
+    
+    jal imprimeVetor
+    
     li $v0, 10
     syscall
 
@@ -72,10 +77,15 @@ inicializaVetor:
     #salva valor aleatorio
     sw $t2, 0($s0)
 
+    #volta o indice	
+    sub $s0, $s0, $t0
+    		
     #carrega argumentos novamente
     add $a0, $zero, $s0
     add $a1, $zero, $s1
     add $a2, $zero, $t2
+    
+    
 
     #salva novoValor
     add $s5, $s5, $t2
@@ -106,5 +116,56 @@ valorAleatorio:
 		sub 	$v1, $t2, $s4   # (a * b + c) % d - e
 	
 		jr      $ra             # retorna 
+		
+# receives a vector and it's size and prints it
+imprimeVetor:
+    # contador do for
+    li $t0, 0
     
+    #$t1 mantem o indice do vetor
+    add $t1, $zero,$a0
+    
+    #tamanho do vetor em $s1
+    add $s1, $zero,$a1
+    
+    # multiplica por 4 para a quantidade de bytes
+    sll $s1,$s1, 2
+
+    for:
+    	#incrementa o indice
+        addi $t0, $t0, 4
+        
+        #checa condicao de parada
+        bgt $t0, $s1, fim_for
+        
+        #carrega valor localizado no vetor
+    	lw $t2, 0($t1)
+    	
+    	#prepara o print
+        li $v0,1
+        
+        #carrega valor no registrador $a0
+        move $a0, $t2
+        
+        #printa
+        syscall
+        
+        #printa espaco em branco
+        li $a0, 32
+        li $v0, 11
+        
+        syscall
+        
+        #incrementa posicao do vetor
+        addi $t1,$t1, 4
+        j for
+        
+    fim_for:
+        li $v0, 4
+        la $a0, nline
+        syscall
+        jr $ra
+        
+
+
     
