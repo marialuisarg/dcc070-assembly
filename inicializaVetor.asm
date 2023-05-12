603f0403705aa285c38ceb95da7d513b65665170
nicolassoam <nsm4456@gmail.com>
2023-05-11 23:23:03 -0300

    a ideia geral ta feita, porem nao se vive no mundo das ideias

--- inicializaVetor.asm
+++ inicializaVetor.asm
@@ -2,7 +2,7 @@
     vet: .word 0:100
     nline: .asciiz "\n"
 
-main:
+.main:
     ## salva numero
     add $a1, 20, $zero
 
@@ -10,13 +10,20 @@ main:
     la $a0, vet
 
     #ultimo valor
-    li $a2, 4
+    li $a2, 5
 
     jal inicializaVetor
+    li $t0, $a0
+
+    li $v0, 10
+    syscall
 
 inicializaVetor:
+    #caso trivial, tam =0
+    bgt	$zero, $a1, end
+
     #prepara stack para 5 valores
-    addi $sp, $sp -36
+    addi $sp, $sp -24
     sw $ra, 0($sp)
     sw $s0, 4($sp)
     sw $s1, 8($sp)
@@ -25,31 +32,50 @@ inicializaVetor:
     add $s0, $zero, $a0
     add $s1 ,$zero, $a1
     add $s2, $zero, $a2
-    
+
+
     #bytes do vetor
     sll $s1, $s1, 2
 
-    bgt	$zero, $a1, end
-
     #tam - 1
-    add $t0, $s1, -1
+    add $t0, $s1, -4
 
-    li $s3, 47
-    li $s4, 97
-    li $s5, 337
-    li $s6, 3
+    move $a0, $s2
+    li $a1, 47
+    li $a2, 97
+    li $a3, 337
+    li $s4, 3
 
-    sw $s3, 16($sp)
-    sw $s4, 20($sp)
-    sw $s5, 24($sp)
-    sw $s6, 28($sp)
+    #carrega quinto argumento
+    sw $s4, 16($sp)
 
     jal valorAleatorio
+    move $t2, $v0
 
-    li $t1, 0
+    #salva valor aleatorio
+    sw $s0, 0($t0)
 
+    add $a0, $zero, $s0
+    add $a1, $zero, $t0
+    add $a2, $zero, $t2
+
+    #salva novoValor
+    add $s5, $s5, $t2
+    sw $s5, 20($sp)
+
+    jal inicializaVetor
 end:
     li $a0, 0
     jr $ra
+
+valorAleatorio:
+		mult 	$a0, $a1        # a * b
+		mflo 	$t0
+		add 	$t1, $t0, $a2   # a * b + c
+		div 	$t1, $a3        # (a * b + c) % d
+		mfhi 	$t2
+		sub 	$v1, $t2, $s4   # (a * b + c) % d - e
+	
+		jr      $ra             # retorna 
     
     
