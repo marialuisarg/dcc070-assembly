434e6f25c70c399bac6705a98260d2f5a7aa1d9c
marialuisarg <marialuisa@ice.ufjf.br>
2023-06-20 16:53:42 -0300

    ultima versao so pra ter certeza

--- oc_trabalho.asm
+++ oc_trabalho.asm
@@ -6,146 +6,146 @@
 .data
     msgSoma:    .asciiz    "Soma: "
 
-# Segmento de texto (instru√ß√µes do programa)
+# Segmento de texto (instruÁıes do programa)
 .text
 main:
-    # Aloca espa√ßo na pilha
+    # Aloca espaÁo na pilha
     addi    $sp, $sp, -80   # 80 bytes para um vetor de 20 inteiros
     
-    # Inicializa vari√°veis locais
-    move    $s0, $sp        # vet aponta para o in√≠cio do vetor na pilha
+    # Inicializa vari·veis locais
+    move    $s0, $sp        # vet aponta para o inÌcio do vetor na pilha
     
-    # Chama a fun√ß√£o inicializaVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
-    li      $a1, 20         # Segundo par√¢metro: SIZE
-    li      $a2, 71         # Terceiro par√¢metro: 71
-    jal     inicializaVetor # Chama a fun√ß√£o inicializaVetor
+    # Chama a funÁ„o inicializaVetor
+    move    $a0, $s0        # Primeiro par‚metro: vet
+    li      $a1, 20         # Segundo par‚metro: SIZE
+    li      $a2, 71         # Terceiro par‚metro: 71
+    jal     inicializaVetor # Chama a funÁ„o inicializaVetor
     li      $s1, 0          # soma = 0
-    move    $s1, $v0        # Guarda o retorno da fun√ß√£o em soma
+    move    $s1, $v0        # Guarda o retorno da funÁ„o em soma
     
-    # Chama a fun√ß√£o imprimeVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
-    jal     imprimeVetor    # Chama a fun√ß√£o imprimeVetor
-    li      $a1, 20         # Segundo par√¢metro: SIZE
+    # Chama a funÁ„o imprimeVetor
+    move    $a0, $s0        # Primeiro par‚metro: vet
+    jal     imprimeVetor    # Chama a funÁ„o imprimeVetor
+    li      $a1, 20         # Segundo par‚metro: SIZE
     
-    # Chama a fun√ß√£o ordenaVetor
-    jal     ordenaVetor     # Chama a fun√ß√£o ordenaVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
-    li      $a1, 20         # Segundo par√¢metro: SIZE
+    # Chama a funÁ„o ordenaVetor
+    jal     ordenaVetor     # Chama a funÁ„o ordenaVetor
+    move    $a0, $s0        # Primeiro par‚metro: vet
+    li      $a1, 20         # Segundo par‚metro: SIZE
     
-    # Chama a fun√ß√£o imprimeVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
-    li      $a1, 20         # Segundo par√¢metro: SIZE
-    jal     imprimeVetor    # Chama a fun√ß√£o imprimeVetor
+    # Chama a funÁ„o imprimeVetor
+    move    $a0, $s0        # Primeiro par‚metro: vet
+    li      $a1, 20         # Segundo par‚metro: SIZE
+    jal     imprimeVetor    # Chama a funÁ„o imprimeVetor
     nop
     
-    # Chama a fun√ß√£o zeraVetor
-    move    $a0, $s0        # Primeiro par√¢metro: &vet[0]
-    addi    $a1, $s0, 80    # Segundo par√¢metro: &vet[20]
-    jal     zeraVetor       # Chama a fun√ß√£o zeraVetor
+    # Chama a funÁ„o zeraVetor
+    move    $a0, $s0        # Primeiro par‚metro: &vet[0]
+    addi    $a1, $s0, 80    # Segundo par‚metro: &vet[20]
+    jal     zeraVetor       # Chama a funÁ„o zeraVetor
     nop
     
-    # Chama a fun√ß√£o imprimeVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
-    li      $a1, 20         # Segundo par√¢metro: SIZE
-    jal     imprimeVetor    # Chama a fun√ß√£o imprimeVetor
+    # Chama a funÁ„o imprimeVetor
+    move    $a0, $s0        # Primeiro par‚metro: vet
+    li      $a1, 20         # Segundo par‚metro: SIZE
+    jal     imprimeVetor    # Chama a funÁ„o imprimeVetor
     nop
     
-    # Impress√£o em tela: printf("Soma: %d\n", soma);
-    li      $v0, 4          # C√≥digo 4 para impress√£o de string
-    la      $a0, msgSoma    # Primeiro par√¢metro: endere√ßo da string "Soma: "
+    # Impress„o em tela: printf("Soma: %d\n", soma);
+    li      $v0, 4          # CÛdigo 4 para impress„o de string
+    la      $a0, msgSoma    # Primeiro par‚metro: endereÁo da string "Soma: "
     syscall
-    li      $v0, 1          # C√≥digo 1 para impress√£o de inteiro
-    move    $a0, $s1        # Primeiro par√¢metro: soma ($s1)
+    li      $v0, 1          # CÛdigo 1 para impress„o de inteiro
+    move    $a0, $s1        # Primeiro par‚metro: soma ($s1)
     syscall
-    li      $v0, 11         # C√≥digo 11 para impress√£o de caractere
-    li      $a0, 10         # Primeiro par√¢metro: \n (ASCII)
+    li      $v0, 11         # CÛdigo 11 para impress„o de caractere
+    li      $a0, 10         # Primeiro par‚metro: \n (ASCII)
     syscall
     
-    # Libera espa√ßo na pilha
-    addi    $sp, $sp, 80    # Libera os 80 bytes alocados pela fun√ß√£o
+    # Libera espaÁo na pilha
+    addi    $sp, $sp, 80    # Libera os 80 bytes alocados pela funÁ„o
 
     # Fim do programa
-    li      $v0, 17         # C√≥digo 17 para exit com valor de retorno
-    li      $a0, 0          # Primeiro par√¢metro: valor de retorno 0
+    li      $v0, 17         # CÛdigo 17 para exit com valor de retorno
+    li      $a0, 0          # Primeiro par‚metro: valor de retorno 0
     syscall 
 
 
 zeraVetor:
-    # Esta fun√ß√£o √© folha
-    # O primeiro par√¢metro √© o ponteiro para o in√≠cio do vetor
-    # O segundo par√¢metro √© ponteiro para o fim do vetor
+    # Esta funÁ„o È folha
+    # O primeiro par‚metro È o ponteiro para o inÌcio do vetor
+    # O segundo par‚metro È ponteiro para o fim do vetor
     
     zeraLoop:
         bge    $a0, $a1, zeraFim    # Se inicio >= fim vai para zeraFim
-        sw      $zero, 0($a0)       # Salva valor 0 no endere√ßo apontado por inicio
-        j       zeraLoop            # Repete o la√ßo
-        addi    $a0, $a0, 4         # Incrementa inicio para a pr√≥xima posi√ß√£o
+        sw      $zero, 0($a0)       # Salva valor 0 no endereÁo apontado por inicio
+        j       zeraLoop            # Repete o laÁo
+        addi    $a0, $a0, 4         # Incrementa inicio para a prÛxima posiÁ„o
     
     zeraFim:
 
-    # Fim da fun√ß√£o    
+    # Fim da funÁ„o    
     jr      $ra             # Retorna
     nop
 
 imprimeVetor:
-    # Esta fun√ß√£o n√£o √© folha
-    # O primeiro par√¢metro √© o ponteiro para o in√≠cio do vetor
-    # O segundo par√¢metro √© o tamanho do vetor
+    # Esta funÁ„o n„o È folha
+    # O primeiro par‚metro È o ponteiro para o inÌcio do vetor
+    # O segundo par‚metro È o tamanho do vetor
     
-    # Aloca espa√ßo na pilha
+    # Aloca espaÁo na pilha
     addi    $sp, $sp, -16   # 16 bytes para $ra, $s0, $s1, $s2
     sw      $ra, 0($sp)     # Salva $ra na pilha
     sw      $s0, 4($sp)     # Salva $s0 na pilha
     sw      $s1, 8($sp)     # Salva $s1 na pilha
     sw      $s2, 12($sp)    # Salva $s2 na pilha
     
-    # Inicializa vari√°veis
-    move    $s0, $a0        # Par√¢metro vet salvo em $s0
-    move    $s1, $a1        # Par√¢metro tam salvo em $s1
-    li      $s2, 0          # Vari√°vel i = 0 em $s2
+    # Inicializa vari·veis
+    move    $s0, $a0        # Par‚metro vet salvo em $s0
+    move    $s1, $a1        # Par‚metro tam salvo em $s1
+    li      $s2, 0          # Vari·vel i = 0 em $s2
     
     imprimeLoop:
         beq     $s2, $s1, imprimeFim    # Se i == tam vai para imprimeFim
-        li      $v0, 1              # C√≥digo 1 para impress√£o de inteiro
+        li      $v0, 1              # CÛdigo 1 para impress„o de inteiro
         
         sll     $t0, $s2, 2         # $t0 = i * 4
         add     $t0, $s0, $t0       # $t0 = &vet[i]
         
         
-        lw      $a0, 0($t0)         # Primeiro par√¢metro: vet[i]
+        lw      $a0, 0($t0)         # Primeiro par‚metro: vet[i]
         syscall
     
-        li      $v0, 11             # C√≥digo 11 para impress√£o de caractere
-        li      $a0, 32             # Primeiro par√¢metro: " " (espa√ßo)
+        li      $v0, 11             # CÛdigo 11 para impress„o de caractere
+        li      $a0, 32             # Primeiro par‚metro: " " (espaÁo)
         
     	addi    $s2, $s2, 1         # Incremento i++
-        j       imprimeLoop         # Repete o la√ßo
+        j       imprimeLoop         # Repete o laÁo
         syscall
               
     imprimeFim:
-    li      $v0, 11         # C√≥digo 11 para impress√£o de caractere
-    li      $a0, 10         # Primeiro par√¢metro: \n
+    li      $v0, 11         # CÛdigo 11 para impress„o de caractere
+    li      $a0, 10         # Primeiro par‚metro: \n
     
     
-    # Libera espa√ßo na pilha
+    # Libera espaÁo na pilha
     lw      $ra, 0($sp)     # Recupera $ra da pilha
     lw      $s0, 4($sp)     # Recupera $s0 da pilha
     lw      $s1, 8($sp)     # Recupera $s1 da pilha
     lw      $s2, 12($sp)    # Recupera $s2 da pilha
-    addi    $sp, $sp, 16    # Libera os 16 bytes alocados pela fun√ß√£o
+    addi    $sp, $sp, 16    # Libera os 16 bytes alocados pela funÁ„o
     
-    # Fim da fun√ß√£o
+    # Fim da funÁ„o
     jr      $ra             # Retorna
     syscall
 
 inicializaVetor:
-    # Esta fun√ß√£o n√£o √© folha
-    # O primeiro par√¢metro √© o ponteiro para o in√≠cio do vetor
-    # O segundo par√¢metro √© o tamanho do vetor
-    # O terceiro par√¢metro √© o √∫ltimo valor aleat√≥rio utilizado na inicializa√ß√£o
+    # Esta funÁ„o n„o È folha
+    # O primeiro par‚metro È o ponteiro para o inÌcio do vetor
+    # O segundo par‚metro È o tamanho do vetor
+    # O terceiro par‚metro È o ˙ltimo valor aleatÛrio utilizado na inicializaÁ„o
 
-    # Aloca espa√ßo na pilha
+    # Aloca espaÁo na pilha
     addi    $sp, $sp, -20   # 20 bytes para $ra, $s0, $s1, $s2 e $s3
     sw      $ra, 0($sp)     # Salva $ra na pilha
     sw      $s0, 4($sp)     # Salva $s0 na pilha
@@ -153,49 +153,49 @@ inicializaVetor:
     sw      $s2, 12($sp)    # Salva $s2 na pilha
     sw      $s3, 16($sp)    # Salva $s3 na pilha
 
-    # Inicializa vari√°veis
-    move    $s1, $a1        # Par√¢metro tamanho salvo em $s1
-    move    $s2, $a2        # Par√¢metro ultimoValor salvo em $s2
+    # Inicializa vari·veis
+    move    $s1, $a1        # Par‚metro tamanho salvo em $s1
+    move    $s2, $a2        # Par‚metro ultimoValor salvo em $s2
     li      $s3, 0          # novoValor = 0
-    move    $s0, $a0        # Par√¢metro vet salvo em $s0
-    # Caso base da recurs√£o
+    move    $s0, $a0        # Par‚metro vet salvo em $s0
+    # Caso base da recurs„o
     move    $v0, $zero      # Prepara valor de retorno 0
     ble     $s1, $zero, inicializaFim   # Se tamanho <= 0 vai para inicializaFim
-    li      $a1, 47         # Segundo par√¢metro: 47 
+    li      $a1, 47         # Segundo par‚metro: 47 
 
     # Passo recursivo
-    # Chama a fun√ß√£o valorAleatorio
-    move    $a0, $s2        # Primeiro par√¢metro: ultimoValor   
-    li      $a2, 97         # Terceir par√¢metro: 97
-    li      $a3, 337        # Quarto par√¢metro: 337
+    # Chama a funÁ„o valorAleatorio
+    move    $a0, $s2        # Primeiro par‚metro: ultimoValor   
+    li      $a2, 97         # Terceir par‚metro: 97
+    li      $a3, 337        # Quarto par‚metro: 337
     
-    addi    $sp, $sp, -4    # Aloca 4 bytes na pilha para o quinto par√¢metro
+    addi    $sp, $sp, -4    # Aloca 4 bytes na pilha para o quinto par‚metro
     li      $t0, 3          # $t0 = 3
      
     jal     valorAleatorio
-    sw      $t0, 0($sp)     # Quinto par√¢metro: 3   
+    sw      $t0, 0($sp)     # Quinto par‚metro: 3   
     addi    $t0, $s1, -1    # $t0 = tamanho - 1
-    move    $s3, $v0        # novoValor = $v0 (retorno da fun√ß√£o valorAleatorio)
+    move    $s3, $v0        # novoValor = $v0 (retorno da funÁ„o valorAleatorio)
     
-    addi    $sp, $sp, 4     # Libera 4 bytes na pilha do quinto par√¢metro
+    addi    $sp, $sp, 4     # Libera 4 bytes na pilha do quinto par‚metro
     
     
     sll     $t0, $t0, 2     # $t0 = (tamanho - 1) * 4
     add     $t0, $s0, $t0   # $t0 = &vet[tamanho - 1]
     sw      $s3, 0($t0)     # vet[tamanho - 1] = novoValor
     
-    # Chama recursivamente a fun√ß√£o inicializaVetor
+    # Chama recursivamente a funÁ„o inicializaVetor
     
-    addi    $a1, $s1, -1    # Segundo par√¢metro: tamanho - 1
-    move    $a2, $s3        # Terceiro par√¢metro: novoValor
+    addi    $a1, $s1, -1    # Segundo par‚metro: tamanho - 1
+    move    $a2, $s3        # Terceiro par‚metro: novoValor
     jal     inicializaVetor
-    move    $a0, $s0        # Primeiro par√¢metro: vet
+    move    $a0, $s0        # Primeiro par‚metro: vet
     
     # Prepara valor de retorno
-    add     $v0, $v0, $s3   # Prepara valor de retorno novoValor + retorno da recurs√£o 
+    add     $v0, $v0, $s3   # Prepara valor de retorno novoValor + retorno da recurs„o 
 
     inicializaFim:
-    # Libera espa√ßo na pilha
+    # Libera espaÁo na pilha
     lw      $ra, 0($sp)     # Recupera $ra da pilha
     lw      $s0, 4($sp)     # Recupera $s0 da pilha
     lw      $s1, 8($sp)     # Recupera $s1 da pilha
@@ -203,17 +203,17 @@ inicializaVetor:
     lw      $s3, 16($sp)    # Recupera $s3 da pilha
     
     
-    # Fim da fun√ß√£o
+    # Fim da funÁ„o
     jr      $ra             # Retorna
-    addi    $sp, $sp, 20    # Libera os 20 bytes alocados pela fun√ß√£o
+    addi    $sp, $sp, 20    # Libera os 20 bytes alocados pela funÁ„o
     
     
 ordenaVetor:
-    # Esta fun√ß√£o n√£o √© folha
-    # O primeiro par√¢metro √© o ponteiro para o in√≠cio do vetor
-    # O segundo par√¢metro √© o tamanho do vetor
+    # Esta funÁ„o n„o È folha
+    # O primeiro par‚metro È o ponteiro para o inÌcio do vetor
+    # O segundo par‚metro È o tamanho do vetor
 
-    # Aloca espa√ßo na pilha
+    # Aloca espaÁo na pilha
     addi    $sp, $sp, -24   # 24 bytes para $ra, $s0, $s1, $s2, $s3 e $s4
     sw      $ra, 0($sp)     # Salva $ra na pilha
     sw      $s0, 4($sp)     # Salva $s0 na pilha
@@ -222,26 +222,26 @@ ordenaVetor:
     sw      $s3, 16($sp)    # Salva $s3 na pilha
     sw      $s4, 20($sp)    # Salva $s4 na pilha
 
-    # Inicializa vari√°veis
-    move    $s0, $a0        # Par√¢metro vet salvo em $s0
-    move    $s1, $a1        # Par√¢metro n salvo em $s1
+    # Inicializa vari·veis
+    move    $s0, $a0        # Par‚metro vet salvo em $s0
+    move    $s1, $a1        # Par‚metro n salvo em $s1
     li      $s3, 0          # j = 0
     li      $s4, 0          # min_idx = 0
     
-    # La√ßo externo
+    # LaÁo externo
     li      $s2, 0          # i = 0
     ordenaFor1:
         addi    $t0, $s1, -1            # $t0 = n - 1
         bge     $s2, $t0, ordenaFim1    # Se i >= n - 1 vai para ordenaFim1
         move    $s4, $s2                # min_idx = i
     
-        # La√ßo interno
+        # LaÁo interno
         addi    $s3, $s2, 1             # j = i + 1
         ordenaFor2:
             bge     $s3, $s1, ordenaFim2    # Se j >= n vai para ordenaFim2
             nop
                     
-            # Condicional dentro do lan√ßo interno
+            # Condicional dentro do lanÁo interno
             # Leitura do valor de vet[j]
             sll     $t0, $s3, 2             # $t0 = j * 4
             add     $t0, $s0, $t0           # $t0 = &vet[j]
@@ -257,45 +257,45 @@ ordenaVetor:
             move    $s4, $s3                # min_idx = j            
             
             sortIf1Fim:
-            j       ordenaFor2              # Repete o la√ßo interno
+            j       ordenaFor2              # Repete o laÁo interno
             addi    $s3, $s3, 1             # j++
         
         ordenaFim2:
-        # Condicional ap√≥s o la√ßo interno
+        # Condicional apÛs o laÁo interno
         beq     $s4, $s2, ordenaIfFim       # Se min_idx == i vai para ordenaIfFim
         nop
         
-        # Chama fun√ß√£o troca
+        # Chama funÁ„o troca
         sll     $t0, $s4, 2             # $t0 = min_idx * 4
-        add     $a0, $s0, $t0           # Primeiro par√¢metro: &vet[min_idx]
+        add     $a0, $s0, $t0           # Primeiro par‚metro: &vet[min_idx]
         sll     $t0, $s2, 2             # $t0 = i * 4
-        add     $a1, $s0, $t0           # Segundo par√¢metro: &vet[i]
+        add     $a1, $s0, $t0           # Segundo par‚metro: &vet[i]
         jal     troca
         
         ordenaIfFim:
         addi    $s2, $s2, 1             # i++
-        j       ordenaFor1              # Repete o la√ßo externo
+        j       ordenaFor1              # Repete o laÁo externo
         nop
     
     ordenaFim1:
-    # Libera espa√ßo na pilha
+    # Libera espaÁo na pilha
     lw      $ra, 0($sp)     # Recupera $ra da pilha
     lw      $s0, 4($sp)     # Recupera $s0 da pilha
     lw      $s1, 8($sp)     # Recupera $s1 da pilha
     lw      $s2, 12($sp)    # Recupera $s2 da pilha
     lw      $s3, 16($sp)    # Recupera $s3 da pilha
     lw      $s4, 20($sp)    # Recupera $s4 da pilha
-    addi    $sp, $sp, 24    # Libera os 24 bytes alocados pela fun√ß√£o
+    addi    $sp, $sp, 24    # Libera os 24 bytes alocados pela funÁ„o
     
-    # Fim da fun√ß√£o
+    # Fim da funÁ„o
     jr      $ra             # Retorna
     nop
        
            
 troca:
-    # Esta fun√ß√£o √© folha
-    # O primeiro par√¢metro √© o ponteiro para a posi√ß√£o a no vetor
-    # O segundo par√¢metro √© o ponteiro para a posi√ß√£o b no vetor
+    # Esta funÁ„o È folha
+    # O primeiro par‚metro È o ponteiro para a posiÁ„o a no vetor
+    # O segundo par‚metro È o ponteiro para a posiÁ„o b no vetor
     
     # Teste da condicional
     beq     $a0, $a1, trocaFim  # Se a == b vai para trocaFim
@@ -307,16 +307,16 @@ troca:
     sw      $t0, 0($a1)     # *b = $t0   
     
     trocaFim:
-    # Fim da fun√ß√£o
+    # Fim da funÁ„o
     jr      $ra             # Retorna
     nop  
                                                                                                                                                                                                                                                           
 valorAleatorio:
-    # Esta fun√ß√£o √© folha
-    # Os quatro primeiros par√¢metros est√£o nos registradores $a0 -- $a3
-    # O quinto par√¢metro est√° na pilha
+    # Esta funÁ„o È folha
+    # Os quatro primeiros par‚metros est„o nos registradores $a0 -- $a3
+    # O quinto par‚metro est· na pilha
     
-    # Recupera o quinto par√¢metro (e) da pilha
+    # Recupera o quinto par‚metro (e) da pilha
     lw      $t0, 0($sp)    # $t0 = e
     
     # Calcula o valor de retorno
@@ -326,6 +326,6 @@ valorAleatorio:
     mfhi    $v0             # $v0 = hi
     
         
-    # Fim da fun√ß√£o
+    # Fim da funÁ„o
     jr      $ra             # Retorna
     sub     $v0, $v0, $t0   # $v0 = (a * b + c) % d - e
