20865fe3d1a395f22bfe1745f971c20a87d22033
marialuisarg <marialuisa@ice.ufjf.br>
2023-06-20 01:30:42 -0300

    ordena não pronta ainda

--- original.asm
+++ original.asm
@@ -10,19 +10,20 @@ main:
     
     # Inicializa variáveis locais
     move    $s0, $sp        # vet aponta para o início do vetor na pilha
+    li      $s1, 0          # soma = 0
     
     # Chama a função inicializaVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
     li      $a2, 71         # Terceiro parâmetro: 71
     jal     inicializaVetor # Chama a função inicializaVetor
-    li      $s1, 0          # soma = 0
     move    $s1, $v0        # Guarda o retorno da função em soma
     
     # Chama a função imprimeVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
-    jal     imprimeVetor    # Chama a função imprimeVetor
     li      $a1, 20         # Segundo parâmetro: SIZE
+    jal     imprimeVetor    # Chama a função imprimeVetor
+    
     # Chama a função ordenaVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
@@ -71,14 +72,14 @@ zeraVetor:
     zeraLoop:
         bge    $a0, $a1, zeraFim    # Se inicio >= fim vai para zeraFim
         sw      $zero, 0($a0)       # Salva valor 0 no endereço apontado por inicio
-        j       zeraLoop            # Repete o laço
         addi    $a0, $a0, 4         # Incrementa inicio para a próxima posição
+        j       zeraLoop            # Repete o laço
     
     zeraFim:
 
     # Fim da função    
     jr      $ra             # Retorna
-    nop
+    
 
 imprimeVetor:
     # Esta função não é folha
@@ -99,26 +100,24 @@ imprimeVetor:
     
     imprimeLoop:
         beq     $s2, $s1, imprimeFim    # Se i == tam vai para imprimeFim
-        li      $v0, 1              # Código 1 para impressão de inteiro
-        
         sll     $t0, $s2, 2         # $t0 = i * 4
         add     $t0, $s0, $t0       # $t0 = &vet[i]
         
-        
+        li      $v0, 1              # Código 1 para impressão de inteiro
         lw      $a0, 0($t0)         # Primeiro parâmetro: vet[i]
         syscall
     
         li      $v0, 11             # Código 11 para impressão de caractere
         li      $a0, 32             # Primeiro parâmetro: " " (espaço)
-        
-    	addi    $s2, $s2, 1         # Incremento i++
-        j       imprimeLoop         # Repete o laço
         syscall
-              
+    
+        addi    $s2, $s2, 1         # Incremento i++
+        j       imprimeLoop         # Repete o laço
+                
     imprimeFim:
     li      $v0, 11         # Código 11 para impressão de caractere
     li      $a0, 10         # Primeiro parâmetro: \n
-    
+    syscall
     
     # Libera espaço na pilha
     lw      $ra, 0($sp)     # Recupera $ra da pilha
@@ -129,7 +128,7 @@ imprimeVetor:
     
     # Fim da função
     jr      $ra             # Retorna
-    syscall
+    
 
 inicializaVetor:
     # Esta função não é folha
@@ -146,42 +145,40 @@ inicializaVetor:
     sw      $s3, 16($sp)    # Salva $s3 na pilha
 
     # Inicializa variáveis
+    move    $s0, $a0        # Parâmetro vet salvo em $s0
     move    $s1, $a1        # Parâmetro tamanho salvo em $s1
     move    $s2, $a2        # Parâmetro ultimoValor salvo em $s2
     li      $s3, 0          # novoValor = 0
-    move    $s0, $a0        # Parâmetro vet salvo em $s0
+
     # Caso base da recursão
     move    $v0, $zero      # Prepara valor de retorno 0
     ble     $s1, $zero, inicializaFim   # Se tamanho <= 0 vai para inicializaFim
-    li      $a1, 47         # Segundo parâmetro: 47 
-
+    
     # Passo recursivo
     # Chama a função valorAleatorio
-    move    $a0, $s2        # Primeiro parâmetro: ultimoValor   
+    move    $a0, $s2        # Primeiro parâmetro: ultimoValor
+    li      $a1, 47         # Segundo parâmetro: 47    
     li      $a2, 97         # Terceir parâmetro: 97
     li      $a3, 337        # Quarto parâmetro: 337
     
     addi    $sp, $sp, -4    # Aloca 4 bytes na pilha para o quinto parâmetro
     li      $t0, 3          # $t0 = 3
-     
+    sw      $t0, 0($sp)     # Quinto parâmetro: 3    
     jal     valorAleatorio
-    sw      $t0, 0($sp)     # Quinto parâmetro: 3   
-    addi    $t0, $s1, -1    # $t0 = tamanho - 1
-    move    $s3, $v0        # novoValor = $v0 (retorno da função valorAleatorio)
-    
     addi    $sp, $sp, 4     # Libera 4 bytes na pilha do quinto parâmetro
     
+    move    $s3, $v0        # novoValor = $v0 (retorno da função valorAleatorio)
     
+    addi    $t0, $s1, -1    # $t0 = tamanho - 1
     sll     $t0, $t0, 2     # $t0 = (tamanho - 1) * 4
     add     $t0, $s0, $t0   # $t0 = &vet[tamanho - 1]
     sw      $s3, 0($t0)     # vet[tamanho - 1] = novoValor
     
     # Chama recursivamente a função inicializaVetor
-    
+    move    $a0, $s0        # Primeiro parâmetro: vet
     addi    $a1, $s1, -1    # Segundo parâmetro: tamanho - 1
     move    $a2, $s3        # Terceiro parâmetro: novoValor
     jal     inicializaVetor
-    move    $a0, $s0        # Primeiro parâmetro: vet
     
     # Prepara valor de retorno
     add     $v0, $v0, $s3   # Prepara valor de retorno novoValor + retorno da recursão 
@@ -193,13 +190,12 @@ inicializaVetor:
     lw      $s1, 8($sp)     # Recupera $s1 da pilha
     lw      $s2, 12($sp)    # Recupera $s2 da pilha
     lw      $s3, 16($sp)    # Recupera $s3 da pilha
-    
+    addi    $sp, $sp, 20    # Libera os 20 bytes alocados pela função
     
     # Fim da função
     jr      $ra             # Retorna
-    addi    $sp, $sp, 20    # Libera os 20 bytes alocados pela função
     
-    
+
 ordenaVetor:
     # Esta função não é folha
     # O primeiro parâmetro é o ponteiro para o início do vetor
@@ -248,8 +244,8 @@ ordenaVetor:
             move    $s4, $s3                # min_idx = j                        
             
             sortIf1Fim:
-            j       ordenaFor2              # Repete o laço interno
             addi    $s3, $s3, 1             # j++
+            j       ordenaFor2              # Repete o laço interno
         
         ordenaFim2:
         # Condicional após o laço interno
@@ -263,8 +259,8 @@ ordenaVetor:
         jal     troca
         
         ordenaIfFim:
-        j       ordenaFor1              # Repete o laço externo
         addi    $s2, $s2, 1             # i++
+        j       ordenaFor1              # Repete o laço externo
     
     ordenaFim1:
     # Libera espaço na pilha
@@ -278,9 +274,7 @@ ordenaVetor:
     
     # Fim da função
     jr      $ra             # Retorna
-    nop
-       
-           
+                  
 troca:
     # Esta função é folha
     # O primeiro parâmetro é o ponteiro para a posição a no vetor
@@ -288,9 +282,9 @@ troca:
     
     # Teste da condicional
     beq     $a0, $a1, trocaFim  # Se a == b vai para trocaFim
-    lw      $t0, 0($a0)     # $t0 = *a
-    # Troca de valores
     
+    # Troca de valores
+    lw      $t0, 0($a0)     # $t0 = *a
     lw      $t1, 0($a1)     # $t1 = *b
     sw      $t1, 0($a0)     # *a = $t1
     sw      $t0, 0($a1)     # *b = $t0
@@ -298,7 +292,7 @@ troca:
     trocaFim:
     # Fim da função
     jr      $ra             # Retorna
-    nop                 
+                      
                                                                                                                                                                                                                                                           
 valorAleatorio:
     # Esta função é folha
@@ -313,8 +307,9 @@ valorAleatorio:
     add     $v0, $v0, $a2   # $v0 = a * b + c
     div     $v0, $a3        # hi = (a * b + c) % d
     mfhi    $v0             # $v0 = hi
-    
+    sub     $v0, $v0, $t0   # $v0 = (a * b + c) % d - e
         
     # Fim da função
     jr      $ra             # Retorna
-    sub     $v0, $v0, $t0   # $v0 = (a * b + c) % d - e
+
+
